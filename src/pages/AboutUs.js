7ccd6ea1bd44b969ea1456335ac87f8714cbe1de
hemant <hemant.mahato004@gmail.com>
2021-06-03 00:38:48 +0530

    Service Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,15 @@
 import React from "react";
 //Page Components
 import AboutSection from "../components/AboutSection";
-
+import ServiceSection from "../components/ServiceSection";
 
 const AboutUs = () => {
   return(
-    <AboutSection />
+    <div>
+      <AboutSection />
+    <ServiceSection />
+    </div>
+    
   )
   
 
